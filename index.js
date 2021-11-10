53183e20eb46d9bc8da3f5fa19bcfe6c3b1075b7
nguyen-huong <mhuong1703@gmail.com>
2021-11-10 15:12:11 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,351 +1,74 @@
-var config = {
-    percent: 0,
-    lat: 0,
-    lng: 0,
-    segX: 14,
-    segY: 12,
-    isHaloVisible: true,
-    isPoleVisible: true,
-    autoSpin: true,
-    zoom: 0,
-
-    skipPreloaderAnimation: false,
-
-    goToHongKong: function() {
-        goTo(22.28552,114.15769);
-    }
-};
-
-var stats;
-var imgs;
-var preloader;
-var preloadPercent;
-var globeDoms;
-var vertices;
-
-var world;
-var worldBg;
-var globe;
-var globeContainer;
-var globePole;
-var globeHalo;
-
-var pixelExpandOffset = 1.5;
-var rX = 0;
-var rY = 0;
-var rZ = 0;
-var sinRX;
-var sinRY;
-var sinRZ;
-var cosRX;
-var cosRY;
-var cosRZ;
-var dragX;
-var dragY;
-var dragLat;
-var dragLng;
-
-var isMouseDown = false;
-var isTweening = false;
-var tick = 1;
-
-var URLS = {
-    bg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/6043/css_globe_bg.jpg',
-    diffuse: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/6043/css_globe_diffuse.jpg',
-    halo: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/6043/css_globe_halo.png',
-};
-
-var transformStyleName = PerspectiveTransform.transformStyleName;
-
-function init(ref) {
-
-    world = document.querySelector('.world');
-    worldBg = document.querySelector('.world-bg');
-    worldBg.style.backgroundImage = 'url(' + URLS.bg + ')';
-    globe = document.querySelector('.world-globe');
-    globeContainer = document.querySelector('.world-globe-doms-container');
-    globePole = document.querySelector('.world-globe-pole');
-    globeHalo = document.querySelector('.world-globe-halo');
-    globeHalo.style.backgroundImage = 'url(' + URLS.halo + ')';
-
-
-    regenerateGlobe();
-
-    var gui = new dat.GUI();
-    gui.add(config, 'lat', -90, 90).listen();
-    gui.add(config, 'lng', -180, 180).listen();
-    gui.add(config, 'isHaloVisible');
-    gui.add(config, 'isPoleVisible');
-    gui.add(config, 'autoSpin');
-    gui.add(config, 'goToHongKong');
-    gui.add(config, 'zoom', 0, 1).listen();
-
-    stats = new Stats();
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.left = 0;
-    stats.domElement.style.top = 0;
-    document.body.appendChild( stats.domElement );
-
-    // events
-    world.ondragstart = function () {return false;};
-    world.addEventListener('mousedown', onMouseDown);
-    world.addEventListener('mousemove', onMouseMove);
-    world.addEventListener('mouseup', onMouseUp);
-    world.addEventListener('touchstart', touchPass(onMouseDown));
-    world.addEventListener('touchmove', touchPass(onMouseMove));
-    world.addEventListener('touchend', touchPass(onMouseUp));
-
-    loop();
-}
-
-function touchPass(func) {
-    return function(evt) {
-        evt.preventDefault();
-        func.call(this, {pageX: evt.changedTouches[0].pageX, pageY: evt.changedTouches[0].pageY});
+document.addEventListener('DOMContentLoaded', () => {
+  /*
+    All audio and images curtosey of archive.org. What a solid website!
+  */
+  const src = [
+    [
+      "John Coltrane", "My Favorite Things", "https://ia803202.us.archive.org/10/items/cd_john-coltrane-my-favorite-things_john-coltrane/disc1/01.%20John%20Coltrane%20-%20My%20Favorite%20Things_sample.mp3", "https://upload.wikimedia.org/wikipedia/en/9/9b/My_Favorite_Things.jpg"
+    ],
+    [
+      "Stan Getz", "Winter Wonderland", "https://ia800100.us.archive.org/20/items/cd_west-coast-live_stan-getz-chet-baker/disc1/01.06.%20Stan%20Getz;%20Chet%20Baker%20-%20Winter%20Wonderland_sample.mp3",
+      "https://ia800100.us.archive.org/20/items/cd_west-coast-live_stan-getz-chet-baker/cd_west-coast-live_stan-getz-chet-baker_itemimage.png"
+    ],
+    [
+      "Monty Alexander", "Pure Imagination", "https://ia800107.us.archive.org/9/items/cd_steamin_monty-alexander/disc1/01.%20Monty%20Alexander%20-%20Pure%20Imagination_sample.mp3", "https://ia800107.us.archive.org/9/items/cd_steamin_monty-alexander/cd_steamin_monty-alexander_itemimage.png"
+    ],
+    [
+      "Ella Fitzgerald", "Sleigh Ride", "https://ia800801.us.archive.org/27/items/cd_ella-wishes-you-a-swinging-christmas_ella-fitzgerald/disc1/05.%20Ella%20Fitzgerald%20-%20Sleigh%20Ride_sample.mp3", "https://ia800801.us.archive.org/27/items/cd_ella-wishes-you-a-swinging-christmas_ella-fitzgerald/cd_ella-wishes-you-a-swinging-christmas_ella-fitzgerald_itemimage.png"
+    ],
+    [
+      "Dave Brubeck", "Greensleeves", "https://ia800705.us.archive.org/16/items/cd_a-dave-brubeck-christmas_dave-brubeck/disc1/07.%20Dave%20Brubeck%20-%20What%20Child%20Is%20This_%20%28Greensleeves%29_sample.mp3", "https://ia800705.us.archive.org/16/items/cd_a-dave-brubeck-christmas_dave-brubeck/cd_a-dave-brubeck-christmas_dave-brubeck_itemimage.png"
+    ]
+  ];
+  
+  for (x = 0; x < src.length; x++) {
+    var s = src[x];
+    var number = parseInt(x) + 1;
+    var artist = document.createTextNode(number + ": " + s[0]);
+    var track_name = document.createTextNode(s[1]);
+    
+    var listItem = document.createElement('div');
+    var artist_text = document.createElement('h3');
+    var track_text = document.createElement('p');
+    
+    artist_text.appendChild(artist);
+    track_text.appendChild(track_name);
+    
+    listItem.appendChild(artist_text);
+    listItem.appendChild(track_text);
+    
+    listItem.classList.add('item');
+    listItem.dataset.index = x;
+    
+    document.getElementById('list').appendChild(listItem);
+  }
+  displayTrack(0);
+  
+  var listItems = document.querySelectorAll('.item');
+  listItems.forEach(el => {
+    el.onclick = () => {
+      displayTrack(el.dataset.index);
     };
-}
-
-function onMouseDown(evt) {
-    isMouseDown = true;
-    dragX = evt.pageX;
-    dragY = evt.pageY;
-    dragLat = config.lat;
-    dragLng = config.lng;
-}
-
-function onMouseMove(evt) {
-    if(isMouseDown) {
-        var dX = evt.pageX - dragX;
-        var dY = evt.pageY - dragY;
-        config.lat = clamp(dragLat + dY * 0.5, -90, 90);
-        config.lng = clampLng(dragLng - dX * 0.5, -180, 180);
+  });
+  
+  function displayTrack(x) {
+    var active = document.querySelector('.is-active');
+    if (active) {
+      active.classList.remove('is-active'); 
     }
-}
-
-function onMouseUp(evt) {
-    if(isMouseDown) {
-        isMouseDown = false;
-    }
-}
-
-function regenerateGlobe() {
-    var dom, domStyle;
-    var x, y;
-    globeDoms = [];
-    while (dom = globeContainer.firstChild) {
-        globeContainer.removeChild(dom);
-    }
-
-    var segX = config.segX;
-    var segY = config.segY;
-    var diffuseImgBackgroundStyle = 'url(' + URLS.diffuse + ')';
-    var segWidth = 1600 / segX | 0;
-    var segHeight = 800 / segY | 0;
-
-    vertices = [];
-
-    var verticesRow;
-    var radius = (536) / 2;
-
-    var phiStart = 0;
-    var phiLength = Math.PI * 2;
-
-    var thetaStart = 0;
-    var thetaLength = Math.PI;
-
-    for ( y = 0; y <= segY; y ++ ) {
-
-        verticesRow = [];
-
-        for ( x = 0; x <= segX; x ++ ) {
-
-            var u = x / segX;
-            var v = 0.05 + y / segY * (1 - 0.1);
-
-            var vertex = {
-                x: - radius * Math.cos( phiStart + u * phiLength ) * Math.sin( thetaStart + v * thetaLength ),
-                y: -radius * Math.cos( thetaStart + v * thetaLength ),
-                z: radius * Math.sin( phiStart + u * phiLength ) * Math.sin( thetaStart + v * thetaLength ),
-                phi: phiStart + u * phiLength,
-                theta: thetaStart + v * thetaLength
-            };
-            verticesRow.push( vertex );
-        }
-        vertices.push( verticesRow );
-    }
-
-    for ( y = 0; y < segY; ++y ) {
-        for ( x = 0; x < segX; ++x ) {
-            dom = document.createElement('div');
-            domStyle = dom.style;
-            domStyle.position = 'absolute';
-            domStyle.width = segWidth + 'px';
-            domStyle.height = segHeight + 'px';
-            domStyle.overflow = 'hidden';
-            domStyle[PerspectiveTransform.transformOriginStyleName] = '0 0';
-            domStyle.backgroundImage = diffuseImgBackgroundStyle;
-            dom.perspectiveTransform = new PerspectiveTransform(dom , segWidth, segHeight);
-            dom.topLeft = vertices[ y ][ x ];
-            dom.topRight = vertices[ y ][ x + 1];
-            dom.bottomLeft = vertices[ y + 1 ][ x ];
-            dom.bottomRight = vertices[ y + 1 ][ x + 1 ];
-            domStyle.backgroundPosition = (-segWidth * x) + 'px ' + (-segHeight * y) + 'px';
-            globeContainer.appendChild(dom);
-            globeDoms.push(dom);
-        }
-    }
-
-}
-
-function loop() {
-    requestAnimationFrame(loop);
-    stats.begin();
-    render();
-    stats.end();
-}
-
-function render() {
-
-    if(config.autoSpin && !isMouseDown && !isTweening) {
-        config.lng = clampLng(config.lng - 0.2);
-    }
-
-    rX = config.lat / 180 * Math. PI;
-    rY = (clampLng(config.lng)  - 270) / 180 * Math. PI;
-
-    globePole.style.display = config.isPoleVisible ? 'block' : 'none';
-    globeHalo.style.display = config.isHaloVisible ? 'block' : 'none';
-
-    var ratio = Math.pow(config.zoom, 1.5);
-    pixelExpandOffset = 1.5 + (ratio) * -1.25;
-    ratio = 1 + ratio * 3;
-    globe.style[transformStyleName] = 'scale3d(' + ratio + ',' + ratio + ',1)';
-    ratio = 1 + Math.pow(config.zoom, 3) * 0.3;
-    worldBg.style[transformStyleName] = 'scale3d(' + ratio + ',' + ratio + ',1)';
-
-    transformGlobe();
-}
-
-function clamp(x, min, max) {
-    return x < min ? min : x > max ? max : x;
-}
-
-function clampLng(lng) {
-    return ((lng + 180) % 360) - 180;
-}
-
-function transformGlobe() {
-
-    var dom, perspectiveTransform;
-    var x, y, v1, v2, v3, v4, vertex, verticesRow, i, len;
-    if(tick ^= 1) {
-        sinRY = Math.sin(rY);
-        sinRX = Math.sin(-rX);
-        sinRZ = Math.sin(rZ);
-        cosRY = Math.cos(rY);
-        cosRX = Math.cos(-rX);
-        cosRZ = Math.cos(rZ);
-
-        var segX = config.segX;
-        var segY = config.segY;
-
-        for ( y = 0; y <= segY; y ++ ) {
-            verticesRow = vertices[y];
-            for ( x = 0; x <= segX; x ++ ) {
-                rotate(vertex = verticesRow[x], vertex.x, vertex.y, vertex.z);
-            }
-        }
-
-        for ( y = 0; y < segY; y ++ ) {
-            for ( x = 0; x < segX; x ++ ) {
-                dom = globeDoms[x + segX * y];
-
-                v1 = dom.topLeft;
-                v2 = dom.topRight;
-                v3 = dom.bottomLeft;
-                v4 = dom.bottomRight;
-
-                expand(v1, v2);
-                expand(v2, v3);
-                expand(v3, v4);
-                expand(v4, v1);
-
-                perspectiveTransform = dom.perspectiveTransform;
-                perspectiveTransform.topLeft.x = v1.tx;
-                perspectiveTransform.topLeft.y = v1.ty;
-                perspectiveTransform.topRight.x = v2.tx;
-                perspectiveTransform.topRight.y = v2.ty;
-                perspectiveTransform.bottomLeft.x = v3.tx;
-                perspectiveTransform.bottomLeft.y = v3.ty;
-                perspectiveTransform.bottomRight.x = v4.tx;
-                perspectiveTransform.bottomRight.y = v4.ty;
-                perspectiveTransform.hasError = perspectiveTransform.checkError();
-
-                if(!(perspectiveTransform.hasError = perspectiveTransform.checkError())) {
-                    perspectiveTransform.calc();
-                }
-            }
-        }
-    } else {
-        for ( i = 0, len = globeDoms.length; i < len; i ++ ) {
-            perspectiveTransform = globeDoms[i].perspectiveTransform;
-            if(!perspectiveTransform.hasError) {
-                perspectiveTransform.update();
-            } else {
-                perspectiveTransform.style[transformStyleName] = 'translate3d(-8192px, 0, 0)';
-            }
-        }
-    }
-}
-
-function goTo(lat, lng) {
-    var dX = lat - config.lat;
-    var dY = lng - config.lng;
-    var roughDistance = Math.sqrt(dX * dX + dY * dY);
-    isTweening = true;
-    TweenMax.to(config, roughDistance * 0.01, {lat: lat, lng: lng, ease:'easeInOutSine'});
-    TweenMax.to(config, 1, {delay: roughDistance * 0.01, zoom: 1, ease:'easeInOutSine', onComplete: function(){
-        isTweening = false;
-    }});
-}
-
-function rotate(vertex, x, y, z) {
-    x0 = x * cosRY - z * sinRY;
-    z0 = z * cosRY + x * sinRY;
-    y0 = y * cosRX - z0 * sinRX;
-    z0 = z0 * cosRX + y * sinRX;
-
-    var offset = 1 + (z0 / 4000);
-    x1 = x0 * cosRZ - y0 * sinRZ;
-    y0 = y0 * cosRZ + x0 * sinRZ;
-
-    vertex.px = x1 * offset;
-    vertex.py = y0 * offset;
-}
-
-// shameless stole and edited from threejs CanvasRenderer
-function expand( v1, v2 ) {
-
-    var x = v2.px - v1.px, y = v2.py - v1.py,
-    det = x * x + y * y, idet;
-
-    if ( det === 0 ) {
-        v1.tx = v1.px;
-        v1.ty = v1.py;
-        v2.tx = v2.px;
-        v2.ty = v2.py;
-        return;
-    }
-
-    idet = pixelExpandOffset / Math.sqrt( det );
-
-    x *= idet; y *= idet;
-
-    v2.tx = v2.px + x;
-    v2.ty = v2.py + y;
-    v1.tx = v1.px - x;
-    v1.ty = v1.py - y;
-
-}
-
-init();
+    var el = document.getElementById('list').children[x];
+    el.classList.add('is-active');
+    var s = src[x],
+        artist = s[0],
+        track = s[1],
+        audio = s[2],
+        img = s[3],
+        number = parseInt(x) + 1;
+    document.getElementById('title').innerText = number + ": " + artist;
+    document.getElementById('song_title').innerText = track;
+    var albumArt = document.getElementById('art');
+    albumArt.src = img;
+    albumArt.alt = artist + " " + track;
+    document.getElementById('audio').src = audio;
+  }
+})
